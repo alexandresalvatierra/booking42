e8f2926f1c12dd17a3dc9b5f4d37d3a9dcac37bb
alexandresalvatierra <alexandre.salvatierra@gmail.com>
2023-11-26 16:23:47 -0300

    feat: add main, footer, header components

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,17 @@
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Properties from './components/Properties'
+
 function App() {
   return (
     <>
-      <header className="m-8">
-        <span className="block text-2xl text-blue-300">Book an Property</span>
-        <span className="block text-lg text-blue-300 font-bold">
-          to go an vacation 😍
-        </span>
-      </header>
-      <main>
-        <h1 className="text-3xl font-bold underline">Hello booking!</h1>
+      <Header />
+      <main className='p-4'>
+        <Properties />
       </main>
-      <footer className="m-8">
-        <section>
-          <span className="block text-md text-blue-300">
-            Dev with ❤️ by{" "}
-            <a
-              href="https://www.linkedin.com/in/alexandre-salvatierra/"
-              className="transition font-bold hover:text-blue-500 duration-300"
-            >
-              Alexandre
-            </a>
-          </span>
-        </section>
-      </footer>
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header className='m-8'>
+      <span className='block text-2xl font-bold text-emerald-400'>
+        Book an Property
+      </span>
+      <span className='block text-lg text-emerald-500'>
+        to go on vacation 😍
+      </span>
+    </header>
+  )
+}
+
+export default Header
